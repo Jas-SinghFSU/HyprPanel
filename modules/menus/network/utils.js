880198c842017175eb23c57d8b4283af945826ba
Jas Singh <jaskiratpal.singh@outlook.com>
2024-07-13 22:34:39 -0700

    Replace network signal icons with labels to prevent stuttering bug.

---
+++ modules/menus/network/utils.js
@@ -0,0 +1,23 @@
+const getWifiIcon = (iconName) => {
+  const deviceIconMap = [
+    ["network-wireless-acquiring", "󰤩"],
+    ["network-wireless-connected", "󰤨"],
+    ["network-wireless-encrypted", "󰤪"],
+    ["network-wireless-hotspot", "󰤨"],
+    ["network-wireless-no-route", "󰤩"],
+    ["network-wireless-offline", "󰤮"],
+    ["network-wireless-signal-excellent", "󰤨"],
+    ["network-wireless-signal-good", "󰤥"],
+    ["network-wireless-signal-ok", "󰤢"],
+    ["network-wireless-signal-weak", "󰤟"],
+    ["network-wireless-signal-none", "󰤯"],
+  ];
+
+  const foundMatch = deviceIconMap.find((icon) =>
+    RegExp(icon[0]).test(iconName.toLowerCase()),
+  );
+
+  return foundMatch ? foundMatch[1] : "󰤨";
+};
+
+export { getWifiIcon };
--- modules/menus/network/wifi/WirelessAPs.js
+++ modules/menus/network/wifi/WirelessAPs.js
@@ -1,3 +1,4 @@
+import { getWifiIcon } from "../utils.js";
 const renderWAPs = (self, network, staging, connecting) => {
   const getIdBySsid = (ssid, nmcliOutput) => {
     const lines = nmcliOutput.trim().split("\n");
@@ -26,194 +27,184 @@ const renderWAPs = (self, network, staging, connecting) => {
     failed: "Connection Failed",
   };
   self.hook(network, () => {
-    console.log(JSON.stringify(network, null, 2));
-    Utils.merge(
-      [staging.bind("value"), connecting.bind("value")],
-      () => {
-        // Sometimes the network service will yield a "this._device is undefined" when
-        // trying to access the "access_points" property. So we must validate that
-        // it's not 'undefined'
-        let WAPs =
-          network.wifi._device !== undefined
-            ? network.wifi["access-points"]
-            : [];
+    Utils.merge([staging.bind("value"), connecting.bind("value")], () => {
+      // Sometimes the network service will yield a "this._device is undefined" when
+      // trying to access the "access_points" property. So we must validate that
+      // it's not 'undefined'
+      let WAPs =
+        network.wifi._device !== undefined ? network.wifi["access-points"] : [];
 
-        const dedupeWAPs = () => {
-          const dedupMap = {};
-          WAPs.forEach((item) => {
-            if (!Object.hasOwnProperty.call(dedupMap, item.ssid)) {
-              dedupMap[item.ssid] = item;
-            }
-          });
-
-          return Object.keys(dedupMap).map((itm) => dedupMap[itm]);
-        };
-
-        WAPs = dedupeWAPs();
-
-        const isInStaging = (wap) => {
-          if (Object.keys(staging.value).length === 0) {
-            return false;
+      const dedupeWAPs = () => {
+        const dedupMap = {};
+        WAPs.forEach((item) => {
+          if (!Object.hasOwnProperty.call(dedupMap, item.ssid)) {
+            dedupMap[item.ssid] = item;
           }
-
-          return wap.bssid === staging.value.bssid;
-        };
-
-        const isDisconnecting = (wap) => {
-          if (wap.ssid === network.wifi.ssid) {
-            return network.wifi.state.toLowerCase() === "deactivating";
-          }
-          return false;
-        };
-
-        const filteredWAPs = WAPs.filter((ap) => {
-          return ap.ssid !== "Unknown" && !isInStaging(ap);
-        }).sort((a, b) => {
-          if (network.wifi.ssid === a.ssid) {
-            return -1;
-          }
-
-          if (network.wifi.ssid === b.ssid) {
-            return 1;
-          }
-
-          return b.strength - a.strength;
         });
 
-        if (
-          filteredWAPs.length <= 0 &&
-          Object.keys(staging.value).length === 0
-        ) {
-          return (self.child = Widget.Label({
-            class_name: "waps-not-found dim",
-            expand: true,
-            hpack: "center",
-            vpack: "center",
-            label: "No Wi-Fi Networks Found",
-          }));
+        return Object.keys(dedupMap).map((itm) => dedupMap[itm]);
+      };
+
+      WAPs = dedupeWAPs();
+
+      const isInStaging = (wap) => {
+        if (Object.keys(staging.value).length === 0) {
+          return false;
         }
-        return (self.children = filteredWAPs.map((ap) => {
-          return Widget.Box({
-            children: [
-              Widget.Button({
-                on_primary_click: () => {
-                  if (ap.bssid === connecting.value || ap.active) {
-                    return;
-                  }
 
-                  connecting.value = ap.bssid;
-                  Utils.execAsync(`nmcli device wifi connect ${ap.bssid}`)
-                    .then(() => {
-                      connecting.value = "";
-                      staging.value = {};
-                    })
-                    .catch((err) => {
-                      if (
-                        err
-                          .toLowerCase()
-                          .includes("secrets were required, but not provided")
-                      ) {
-                        staging.value = ap;
-                      } else {
-                        Utils.notify({
-                          summary: "Network",
-                          body: err,
-                          timeout: 5000,
-                        });
-                      }
-                      connecting.value = "";
-                    });
-                },
-                class_name: "network-element-item",
-                child: Widget.Box({
-                  hexpand: true,
-                  children: [
-                    Widget.Box({
-                      hpack: "start",
-                      hexpand: true,
-                      children: [
-                        Widget.Icon({
-                          class_name: `network-icon wifi ${ap.ssid === network.wifi.ssid ? "active" : ""}`,
-                          icon: `${ap["iconName"]}`,
-                        }),
-                        Widget.Box({
-                          class_name: "connection-container",
-                          vertical: true,
-                          children: [
-                            Widget.Label({
-                              class_name: "active-connection",
-                              hpack: "start",
-                              truncate: "end",
-                              wrap: true,
-                              label: ap.ssid,
-                            }),
-                            Widget.Revealer({
-                              revealChild: ap.ssid === network.wifi.ssid,
-                              child: Widget.Label({
-                                hpack: "start",
-                                class_name: "connection-status dim",
-                                label:
-                                  WifiStatusMap[
-                                  network.wifi.state.toLowerCase()
-                                  ],
-                              }),
-                            }),
-                          ],
-                        }),
-                      ],
-                    }),
-                    Widget.Revealer({
-                      hpack: "end",
-                      vpack: "start",
-                      reveal_child:
-                      ap.bssid === connecting.value || isDisconnecting(ap),
-                      child: Widget.Spinner({
+        return wap.bssid === staging.value.bssid;
+      };
+
+      const isDisconnecting = (wap) => {
+        if (wap.ssid === network.wifi.ssid) {
+          return network.wifi.state.toLowerCase() === "deactivating";
+        }
+        return false;
+      };
+
+      const filteredWAPs = WAPs.filter((ap) => {
+        return ap.ssid !== "Unknown" && !isInStaging(ap);
+      }).sort((a, b) => {
+        if (network.wifi.ssid === a.ssid) {
+          return -1;
+        }
+
+        if (network.wifi.ssid === b.ssid) {
+          return 1;
+        }
+
+        return b.strength - a.strength;
+      });
+
+      if (filteredWAPs.length <= 0 && Object.keys(staging.value).length === 0) {
+        return (self.child = Widget.Label({
+          class_name: "waps-not-found dim",
+          expand: true,
+          hpack: "center",
+          vpack: "center",
+          label: "No Wi-Fi Networks Found",
+        }));
+      }
+      return (self.children = filteredWAPs.map((ap) => {
+        return Widget.Box({
+          children: [
+            Widget.Button({
+              on_primary_click: () => {
+                if (ap.bssid === connecting.value || ap.active) {
+                  return;
+                }
+
+                connecting.value = ap.bssid;
+                Utils.execAsync(`nmcli device wifi connect ${ap.bssid}`)
+                  .then(() => {
+                    connecting.value = "";
+                    staging.value = {};
+                  })
+                  .catch((err) => {
+                    if (
+                      err
+                        .toLowerCase()
+                        .includes("secrets were required, but not provided")
+                    ) {
+                      staging.value = ap;
+                    } else {
+                      Utils.notify({
+                        summary: "Network",
+                        body: err,
+                        timeout: 5000,
+                      });
+                    }
+                    connecting.value = "";
+                  });
+              },
+              class_name: "network-element-item",
+              child: Widget.Box({
+                hexpand: true,
+                children: [
+                  Widget.Box({
+                    hpack: "start",
+                    hexpand: true,
+                    children: [
+                      Widget.Label({
                         vpack: "start",
-                        class_name: "spinner wap",
+                        class_name: `network-icon wifi ${ap.ssid === network.wifi.ssid ? "active" : ""}`,
+                        label: getWifiIcon(`${ap["iconName"]}`),
                       }),
+                      Widget.Box({
+                        class_name: "connection-container",
+                        vpack: "center",
+                        vertical: true,
+                        children: [
+                          Widget.Label({
+                            vpack: "center",
+                            class_name: "active-connection",
+                            hpack: "start",
+                            truncate: "end",
+                            wrap: true,
+                            label: ap.ssid,
+                          }),
+                          Widget.Revealer({
+                            revealChild: ap.ssid === network.wifi.ssid,
+                            child: Widget.Label({
+                              hpack: "start",
+                              class_name: "connection-status dim",
+                              label:
+                                WifiStatusMap[network.wifi.state.toLowerCase()],
+                            }),
+                          }),
+                        ],
+                      }),
+                    ],
+                  }),
+                  Widget.Revealer({
+                    hpack: "end",
+                    vpack: "start",
+                    reveal_child:
+                      ap.bssid === connecting.value || isDisconnecting(ap),
+                    child: Widget.Spinner({
+                      vpack: "start",
+                      class_name: "spinner wap",
                     }),
-                  ],
-                }),
+                  }),
+                ],
               }),
-              Widget.Revealer({
-                vpack: "start",
-                reveal_child: ap.bssid !== connecting.value && ap.active,
-                child: Widget.Button({
-                  tooltip_text: "Delete/Forget Network",
-                  class_name: "menu-icon-button network disconnect",
-                  on_primary_click: () => {
-                    connecting.value = ap.bssid;
+            }),
+            Widget.Revealer({
+              vpack: "start",
+              reveal_child: ap.bssid !== connecting.value && ap.active,
+              child: Widget.Button({
+                tooltip_text: "Delete/Forget Network",
+                class_name: "menu-icon-button network disconnect",
+                on_primary_click: () => {
+                  connecting.value = ap.bssid;
+                  Utils.execAsync("nmcli connection show --active").then(() => {
                     Utils.execAsync("nmcli connection show --active").then(
-                      () => {
-                        Utils.execAsync("nmcli connection show --active").then(
-                          (res) => {
-                            const connectionId = getIdBySsid(ap.ssid, res);
+                      (res) => {
+                        const connectionId = getIdBySsid(ap.ssid, res);
 
-                            Utils.execAsync(
-                              `nmcli connection delete ${connectionId} "${ap.ssid}"`,
-                            )
-                              .then(() => (connecting.value = ""))
-                              .catch((err) => {
-                                connecting.value = "";
-                                console.error(
-                                  `Error while forgetting "${ap.ssid}": ${err}`,
-                                );
-                              });
-                          },
-                        );
+                        Utils.execAsync(
+                          `nmcli connection delete ${connectionId} "${ap.ssid}"`,
+                        )
+                          .then(() => (connecting.value = ""))
+                          .catch((err) => {
+                            connecting.value = "";
+                            console.error(
+                              `Error while forgetting "${ap.ssid}": ${err}`,
+                            );
+                          });
                       },
                     );
-                  },
-                  child: Widget.Label({
-                    label: "󰚃"
-                  }),
+                  });
+                },
+                child: Widget.Label({
+                  label: "󰚃",
                 }),
               }),
-            ],
-          });
-        }));
-      },
-    );
+            }),
+          ],
+        });
+      }));
+    });
   });
 };
 
