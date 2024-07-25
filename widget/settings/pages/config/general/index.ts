7fddcf0299a947ef5bca6b9ec50f7237fc2dbd40
Jas Singh <jaskiratpal.singh@outlook.com>
2024-07-24 22:44:35 -0700

    Implemented the ability to specify the terminal to launch btop with.

--- widget/settings/pages/config/general/index.ts
+++ widget/settings/pages/config/general/index.ts
@@ -12,6 +12,7 @@ export const BarGeneral = () => {
             Option({ opt: options.theme.font.name, title: 'Font', type: 'font' }),
             Option({ opt: options.theme.font.size, title: 'Font Size', type: 'string' }),
             Option({ opt: options.theme.font.weight, title: 'Font Weight', subtitle: "100, 200, 300, etc.", type: 'number' }),
+            Option({ opt: options.terminal, title: 'Terminal', subtitle: "Tools such as 'btop' will open in this terminal", type: 'string' }),
         ]
     })
 }
