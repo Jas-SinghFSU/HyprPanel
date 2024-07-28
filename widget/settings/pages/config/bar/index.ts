1af4909277c5650e5039d0c2987abba2014f4914
Jas Singh <jaskiratpal.singh@outlook.com>
2024-07-28 11:59:11 -0700

    Move button spacing config into the Configuration section instead of Theming.

--- widget/settings/pages/config/bar/index.ts
+++ widget/settings/pages/config/bar/index.ts
@@ -15,6 +15,8 @@ export const BarSettings = () => {
                 Option({ opt: options.bar.layouts, title: 'Bar Layouts for Monitors', subtitle: 'Please refer to the github README for instructions: https://github.com/Jas-SinghFSU/HyprPanel', type: 'object' }, 'bar-layout-input'),
 
                 Header('Spacing'),
+                Option({ opt: options.theme.bar.buttons.spacing, title: 'Button Spacing', type: 'string' }),
+                Option({ opt: options.theme.bar.buttons.radius, title: 'Button Radius', type: 'string' }),
                 Option({ opt: options.theme.bar.floating, title: 'Floating Bar', type: 'boolean' }),
                 Option({ opt: options.theme.bar.margin_top, title: 'Margin Top', subtitle: 'Only applies if floating is enabled', type: 'string' }),
                 Option({ opt: options.theme.bar.margin_bottom, title: 'Margin Bottom', subtitle: 'Only applies if floating is enabled', type: 'string' }),
