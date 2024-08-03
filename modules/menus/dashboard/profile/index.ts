f6bef4140d2a6a23c27787e705753f9297d8fe5a
matavach <erik@matijevich.org>
2024-08-03 09:49:22 -0500

    Update modules/menus/dashboard/profile/index.ts
    Co-authored-by: Jas Singh <jaskiratpal.singh@outlook.com>

--- modules/menus/dashboard/profile/index.ts
+++ modules/menus/dashboard/profile/index.ts
@@ -26,7 +26,7 @@ const Profile = () => {
                         hpack: "center",
                         class_name: "profile-picture",
                         icon: image.bind("value").as(i => {
-                            try{
+                            try {
                                 GdkPixbuf.Pixbuf.new_from_file(i)
                                 return i
                             } catch {
