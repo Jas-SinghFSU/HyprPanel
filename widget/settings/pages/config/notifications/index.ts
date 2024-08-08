233f9229720dab55d52e90a66491f975812b266e
Jas Singh <jaskiratpal.singh@outlook.com>
2024-08-07 21:52:43 -0700

    Update widget/settings/pages/config/notifications/index.ts

--- widget/settings/pages/config/notifications/index.ts
+++ widget/settings/pages/config/notifications/index.ts
@@ -9,7 +9,7 @@ export const NotificationSettings = () => {
         vertical: true,
         children: [
             Header('Notification Settings'),
-            Option({ opt: options.notifications.position, title: 'Notification Location', type: 'enum', enums: ['top left', 'top', 'top right', 'bottom right', 'bottom', 'bottom left'] }),
+            Option({ opt: options.notifications.position, title: 'Notification Location', type: 'enum', enums: ['top left', 'top', 'top right', 'right', 'bottom right', 'bottom', 'bottom left', 'left'] }),
             Option({ opt: options.notifications.monitor, title: 'Monitor', subtitle: 'The ID of the monitor on which to display the notification', type: 'number' }),
             Option({ opt: options.notifications.active_monitor, title: 'Follow Cursor', subtitle: 'The notification will follow the monitor of your cursor', type: 'boolean' }),
             Option({ opt: options.notifications.timeout, title: 'Notification Timeout', subtitle: 'How long notification popups will last (in milliseconds).', type: 'number' }),
