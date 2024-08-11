b69f56df8c0f1dcc7148810340ab343b9c162cf2
Jas Singh <jaskiratpal.singh@outlook.com>
2024-08-10 18:31:27 -0700

    Revert this to how it was before since its exactly the same thing.

--- lib/utils.ts
+++ lib/utils.ts
@@ -55,7 +55,7 @@ export async function sh(cmd: string | string[]) {
 
 export function forMonitors(widget: (monitor: number) => Gtk.Window) {
     const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
-    return range(n, 0).flatMap((monitor) => widget(monitor));
+    return range(n, 0).flatMap(widget);
 }
 
 /**
