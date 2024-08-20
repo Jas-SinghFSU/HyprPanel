2aef35afd8278b543ddb48d92db051d34c61a394
Noam Stolero <noam.stolero@gmail.com>
2024-08-20 22:37:03 +0300

    Fix build failure with flakes (#158)
    Signed-off-by: Noam Stolero <noamstolero@gmail.com>
    Co-authored-by: Noam Stolero <noamstolero@gmail.com>

--- nix/default.nix
+++ nix/default.nix
@@ -1,36 +1,36 @@
-{ inputs
-, pkgs
-, system
-, stdenv
-, lib
-, writeShellScriptBin
-, bun
-, dart-sass
-, fd
-, accountsservice
-, btop
-, pipewire
-, bluez
-, bluez-tools
-, grimblast
-, gpu-screen-recorder
-, networkmanager
-, brightnessctl
-, matugen
-, swww
-, python3
-, gnome
-}:
-let
+{
+  inputs,
+  pkgs,
+  system,
+  stdenv,
+  lib,
+  writeShellScriptBin,
+  bun,
+  dart-sass,
+  fd,
+  accountsservice,
+  btop,
+  pipewire,
+  bluez,
+  bluez-tools,
+  grimblast,
+  gpu-screen-recorder,
+  networkmanager,
+  brightnessctl,
+  matugen,
+  swww,
+  python3,
+  gnome,
+}: let
   ags = inputs.ags.packages.${system}.default.override {
-    extraPackages = [ accountsservice ];
+    extraPackages = [accountsservice];
   };
 
   pname = "hyprpanel";
   config = stdenv.mkDerivation {
     inherit pname;
     version = "latest";
-    src = ./.;
+    src = ../.;
 
     buildPhase = ''
       ${bun}/bin/bun build ./main.ts \
@@ -48,12 +48,11 @@ let
       cp -f main.js $out/config.js
     '';
   };
-in
-{
+in {
   desktop = {
     inherit config;
     script = writeShellScriptBin pname ''
-      export PATH=$PATH:${lib.makeBinPath [dart-sass fd btop pipewire bluez bluez-tools grimblast gpu-screen-recorder brightnessctl gnome.gnome-bluetooth python3]}
+      export PATH=$PATH:${lib.makeBinPath [dart-sass fd btop pipewire bluez bluez-tools networkmanager matugen swww grimblast gpu-screen-recorder brightnessctl gnome.gnome-bluetooth python3]}
             ${ags}/bin/ags -b hyprpanel -c ${config}/config.js $@
     '';
   };
