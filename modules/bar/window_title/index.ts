628c81e4ffc00ae952d9cfb6608b4658a141e8a1
Jas Singh <jaskiratpal.singh@outlook.com>
2024-08-19 20:54:21 -0700

    Update modules/bar/window_title/index.ts

--- modules/bar/window_title/index.ts
+++ modules/bar/window_title/index.ts
@@ -18,7 +18,6 @@ const filterTitle = (windowtitle: ActiveClient) => {
         ["spotify", "󰓇", "Spotify"],
         ["ristretto", "󰋩", "Ristretto"],
         ["obsidian", "󱓧", "Obsidian"],
-      
         // Browsers
         ["google-chrome", "", "Google Chrome"],
         ["brave-browser", "󰖟", "Brave Browser"],
