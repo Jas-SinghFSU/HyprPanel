056311a147bfa96bebfb10bff26ed3347e3798c1
Jas Singh <jaskiratpal.singh@outlook.com>
2024-08-25 02:23:06 -0700

    Added the ability to configure the notification radius

--- widget/settings/pages/config/notifications/index.ts
+++ widget/settings/pages/config/notifications/index.ts
@@ -12,6 +12,7 @@ export const NotificationSettings = () => {
             children: [
                 Header('Notification Settings'),
                 Option({ opt: options.notifications.position, title: 'Notification Location', type: 'enum', enums: ['top left', 'top', 'top right', 'right', 'bottom right', 'bottom', 'bottom left', 'left'] }),
+                Option({ opt: options.theme.notification.border_radius, title: 'Border Radius', type: 'string' }),
                 Option({ opt: options.notifications.monitor, title: 'Monitor', subtitle: 'The ID of the monitor on which to display the notification', type: 'number' }),
                 Option({ opt: options.notifications.active_monitor, title: 'Follow Cursor', subtitle: 'The notification will follow the monitor of your cursor', type: 'boolean' }),
                 Option({ opt: options.notifications.timeout, title: 'Notification Timeout', subtitle: 'How long notification popups will last (in milliseconds).', type: 'number' }),
