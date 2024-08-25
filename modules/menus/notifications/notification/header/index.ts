5db6eb336e740623d6d3ecdf2481648a16dad34f
Jas Singh <jaskiratpal.singh@outlook.com>
2024-08-25 03:48:07 -0700

    24hr time switch set in the Clock Menu now effects notification time also. (#184)

--- modules/menus/notifications/notification/header/index.ts
+++ modules/menus/notifications/notification/header/index.ts
@@ -2,11 +2,15 @@ import GLib from "gi://GLib";
 import { Notification } from "types/service/notifications";
 import { NotificationIcon } from "./icon.js";
 import { notifHasImg } from "../../utils.js";
+import options from "options.js";
 
-const time = (time: number, format = "%I:%M %p") =>
-    GLib.DateTime.new_from_unix_local(time).format(format);
+const { military } = options.menus.clock.time;
 
 export const Header = (notif: Notification) => {
+    const time = (time: number, format = "%I:%M %p") => {
+        return GLib.DateTime.new_from_unix_local(time).format(military.value ? "%H:%M" : format);
+    }
+
     return Widget.Box({
         vertical: false,
         hexpand: true,
