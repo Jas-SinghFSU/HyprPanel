fa4cd8cfea17aa875a9daab9ebe2cd822575e828
Zoran Olujic <zoran.olujic@beyonnex.com>
2024-09-06 11:31:53 +0200

    Support for apt and ubuntu in CheckUpdates script

--- scripts/checkUpdates.sh
+++ scripts/checkUpdates.sh
@@ -5,9 +5,17 @@ check_arch_updates() {
     echo "$result"
 }
 
+check_ubuntu_updates() {
+  result=$(apt-get -s -o Debug::NoLocking=true upgrade | grep -c ^Inst)
+  echo "$result"
+}
+
 case "$1" in
 -arch)
     check_arch_updates
+    ;;
+-ubuntu)
+    check_ubuntu_updates
     ;;
 *)
     echo "0"
