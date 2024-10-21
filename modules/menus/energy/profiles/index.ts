e111ec5ec96686c2fdd8e79a0864693dd65895c7
Jas Singh <jaskiratpal.singh@outlook.com>
2024-10-21 01:39:16 -0700

    Update modules/menus/energy/profiles/index.ts

--- modules/menus/energy/profiles/index.ts
+++ modules/menus/energy/profiles/index.ts
@@ -32,7 +32,7 @@ const EnergyProfiles = (): BoxWidget => {
                     Widget.Label({
                         class_name: 'menu-label uptime',
                         label: uptime.bind().as(renderUptime),
-                        tooltipText: 'Uptime'
+                        tooltipText: 'Uptime',
                     }),
                 ],
             }),
