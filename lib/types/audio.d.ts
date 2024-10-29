cae1b98fb5ed988fb39041f0489b88132ad5f1c2
Jas Singh <jaskiratpal.singh@outlook.com>
2024-10-28 23:46:54 -0700

    Organize media player code

--- lib/types/audio.d.ts
+++ lib/types/audio.d.ts
@@ -3,3 +3,12 @@ export type InputDevices = Button<Box<Box<Label<Attribute>, Attribute>, Attribut
 type DummyDevices = Button<Box<Box<Label<Attribute>, Attribute>, Attribute>, Attribute>[];
 type RealPlaybackDevices = Button<Box<Box<Label<Attribute>, Attribute>, Attribute>, Attribute>[];
 export type PlaybackDevices = DummyDevices | RealPlaybackDevices;
+
+export type MediaTags = {
+    title: string;
+    artists: string;
+    artist: string;
+    album: string;
+    name: string;
+    identity: string;
+};
