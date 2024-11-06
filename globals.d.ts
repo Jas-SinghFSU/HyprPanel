f4dfdb573813b8a1c9fe23c844d3bdb772cd16b7
Jas Singh <jaskiratpal.singh@outlook.com>
2024-11-05 21:09:15 -0800

    Provide a cli command to clear notifications.

--- globals.d.ts
+++ globals.d.ts
@@ -7,6 +7,8 @@ declare global {
     var useTheme: (filePath: string) => void;
     var getSystrayItems: () => string;
     var isWindowVisible: (windowName: string) => boolean;
+    var clearAllNotifications: () => Promise<void>;
+
     var globalWeatherVar: VariableType<Weather>;
     var options: Options;
     var removingNotifications: VariableType<boolean>;
