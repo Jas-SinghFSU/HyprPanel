5bf64890fc69ec58f1d7053e5cdf79f5bc423d8f
Jas Singh <jaskiratpal.singh@outlook.com>
2024-11-09 11:33:34 -0800

    wip

---
+++ greeter/greeter.ts
@@ -0,0 +1,29 @@
+import './session';
+import GLib from 'gi://GLib?version=2.0';
+import RegularWindow from 'widget/RegularWindow';
+import auth from './auth';
+
+const win = RegularWindow({
+    name: 'greeter',
+    setup: (self) => {
+        self.set_default_size(500, 500);
+        self.show_all();
+        auth.attribute.password.grab_focus();
+    },
+    child: Widget.Overlay({
+        child: Widget.Box({ expand: true }),
+        overlays: [
+            Widget.Box({
+                vpack: 'center',
+                hpack: 'center',
+                child: auth,
+            }),
+        ],
+    }),
+});
+
+App.config({
+    icons: './assets',
+    windows: [win],
+    cursorTheme: GLib.getenv('XCURSOR_THEME')!,
+});
