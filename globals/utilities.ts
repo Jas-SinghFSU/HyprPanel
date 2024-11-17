b819331dfae38e09a977566888799bedc0372dc7
Jas Singh <jaskiratpal.singh@outlook.com>
2024-11-17 12:34:44 -0800

    Added a CLI command to update layout.

--- globals/utilities.ts
+++ globals/utilities.ts
@@ -1,3 +1,5 @@
+import options from 'options';
+
 globalThis.isWindowVisible = (windowName: string): boolean => {
     const appWindow = App.getWindow(windowName);
 
@@ -5,4 +7,18 @@ globalThis.isWindowVisible = (windowName: string): boolean => {
         return false;
     }
     return appWindow.visible;
+};
+
+globalThis.setLayout = (layout: string): string => {
+    console.log(layout);
+
+    try {
+        const layoutJson = JSON.parse(layout);
+        const { layouts } = options.bar;
+
+        layouts.value = layoutJson;
+        return 'Successfully updated layout.';
+    } catch (error) {
+        return `Failed to set layout: ${error}`;
+    }
 };
