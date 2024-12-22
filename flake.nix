fe7f80c419d34642c765eb09fa7a7081c1c03677
ThomasCarey4 <scarey.tcarey@gmail.com>
2024-12-22 04:12:51 +0000

    Changed the nix install to use ags bundler

--- flake.nix
+++ flake.nix
@@ -1,78 +1,62 @@
 {
-  description = "A Bar/Panel for Hyprland with extensive customizability.";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    ags.url = "github:Aylur/ags";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    ags.url = "github:aylur/ags";
+    astal.url = "github:aylur/astal";
   };
 
-  outputs = inputs: let
+  outputs = { self, nixpkgs, ags, astal }: let
     systems = [
       "x86_64-linux"
       "x86_64-darwin"
       "aarch64-darwin"
       "aarch64-linux"
     ];
-    forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
-    pkgsFor = forEachSystem (
-      system:
-        import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        }
-    );
-
-    devShellFor = system:
-      inputs.nixpkgs.lib.genAttrs ["default"] (
-        _:
-          inputs.nixpkgs.legacyPackages.${system}.mkShell {
-            buildInputs = [
-              pkgsFor.${system}.esbuild
-              pkgsFor.${system}.fish
-              pkgsFor.${system}.typescript
-              pkgsFor.${system}.bun
-              pkgsFor.${system}.libnotify
-              pkgsFor.${system}.dart-sass
-              pkgsFor.${system}.fd
-              pkgsFor.${system}.btop
-              pkgsFor.${system}.bluez
-              pkgsFor.${system}.libgtop
-              pkgsFor.${system}.gobject-introspection
-              pkgsFor.${system}.glib
-              pkgsFor.${system}.bluez-tools
-              pkgsFor.${system}.grimblast
-              pkgsFor.${system}.gpu-screen-recorder
-              pkgsFor.${system}.brightnessctl
-              pkgsFor.${system}.gnome-bluetooth
-              pkgsFor.${system}.python3
-              pkgsFor.${system}.matugen
-              inputs.ags.packages.${system}.agsWithTypes
-            ];
-            nativeBuildInputs = with pkgsFor.${system}; [
-              nixfmt-rfc-style
-              nil
-            ];
-            shellHook = ''
-              export GDK_BACKEND=wayland
-              export GI_TYPELIB_PATH=${pkgsFor.${system}.libgtop}/lib/girepository-1.0:${pkgsFor.${system}.glib}/lib/girepository-1.0:$GI_TYPELIB_PATH
-            '';
-          }
-      );
+    forEachSystem = nixpkgs.lib.genAttrs systems;
   in {
-    devShells = forEachSystem devShellFor;
+    packages = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = ags.lib.bundle {
+        inherit pkgs;
+        src = ./.;
+        name = "hyprpanel";
+        entry = "app.ts";
 
-    overlay = final: prev: {
-      hyprpanel =
-        if final ? callPackage
-        then (final.callPackage ./nix {inherit inputs;}).desktop.script
-        else inputs.self.packages.${prev.stdenv.system}.default;
-    };
-    packages = forEachSystem (
-      system: let
-        pkgs = pkgsFor.${system};
-      in {
-        default = (pkgs.callPackage ./nix {inherit inputs;}).desktop.script;
-      }
-    );
+        extraPackages = [
+          ags.packages.${system}.agsFull
+          astal.packages.${system}.tray
+          astal.packages.${system}.hyprland
+          astal.packages.${system}.io
+          astal.packages.${system}.apps
+          astal.packages.${system}.battery
+          astal.packages.${system}.bluetooth
+          astal.packages.${system}.mpris
+          astal.packages.${system}.network
+          astal.packages.${system}.notifd
+          astal.packages.${system}.powerprofiles
+          astal.packages.${system}.wireplumber
+          pkgs.esbuild
+          pkgs.fish
+          pkgs.typescript
+          pkgs.bun
+          pkgs.libnotify
+          pkgs.dart-sass
+          pkgs.fd
+          pkgs.btop
+          pkgs.bluez
+          pkgs.libgtop
+          pkgs.gobject-introspection
+          pkgs.glib
+          pkgs.bluez-tools
+          pkgs.grimblast
+          pkgs.gpu-screen-recorder
+          pkgs.brightnessctl
+          pkgs.gnome-bluetooth
+          pkgs.python3
+          pkgs.matugen
+        ];
+      };
+    });
   };
 }
