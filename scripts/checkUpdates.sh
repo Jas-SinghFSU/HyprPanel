f6942277cda49d8838c671dd9efc54e768593a3c
ilikestreet <ilikestreet.jz@gmail.com>
2024-12-26 14:29:56 +0800

    feat: add flatpak update count

--- scripts/checkUpdates.sh
+++ scripts/checkUpdates.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
+check_flatpak_updates() {
+  flatpak_updates=0
+  if [[ ! -x "command -v flatpak" ]]; then
+    flatpak_updates=$(flatpak update | grep -E "^[[:blank:]][0-9]{1}\." | wc -l)
+  fi
+  echo $flatpak_updates
+}
+
 check_arch_updates() {
-    official_updates=0
-    aur_updates=0
+  official_updates=0
+  aur_updates=0
+  flatpak_updates=$(check_flatpak_updates)
+  
 	if command -v paru &> /dev/null; then
 		aur_helper="paru"
 	else
 		aur_helper="yay"
 	fi
 
-    if [ "$1" = "-y" ]; then
-        aur_updates=$($aur_helper -Qum 2>/dev/null | wc -l)
-    elif [ "$1" = "-p" ]; then
-        official_updates=$(checkupdates 2>/dev/null | wc -l)
-    else
-        official_updates=$(checkupdates 2>/dev/null | wc -l)
-        aur_updates=$($aur_helper -Qum 2>/dev/null | wc -l)
-    fi
+  if [ "$1" = "-y" ]; then
+    aur_updates=$($aur_helper -Qum 2>/dev/null | wc -l)
+  elif [ "$1" = "-p" ]; then
+    official_updates=$(checkupdates 2>/dev/null | wc -l)
+  else
+    official_updates=$(checkupdates 2>/dev/null | wc -l)
+    aur_updates=$($aur_helper -Qum 2>/dev/null | wc -l)
+  fi
 
-    total_updates=$((official_updates + aur_updates))
+  total_updates=$((official_updates + aur_updates + flatpak_updates))
 
-    echo $total_updates
+  echo $total_updates
 }
 
 check_ubuntu_updates() {
@@ -42,6 +52,9 @@ case "$1" in
     ;;
 -fedora)
     check_fedora_updates
+    ;;
+-flatpak)
+    check_flatpak_updates
     ;;
 *)
     echo "0"
