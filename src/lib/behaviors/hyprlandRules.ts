7204ba657b32d479ed28d9c9bdefdff581b4ae43
Jas Singh <jaskiratpal.singh@outlook.com>
2024-12-31 02:17:23 -0800

    Fix startup window rules. (#682)

--- src/lib/behaviors/hyprlandRules.ts
+++ src/lib/behaviors/hyprlandRules.ts
@@ -1,18 +1,18 @@
 import { hyprlandService } from '../constants/services';
 
 const floatSettingsDialog = (): void => {
-    hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^(hyprpanel-settings)$"`);
+    hyprlandService.message(`keyword windowrulev2 float, title:^(hyprpanel-settings)$`);
 
     hyprlandService.connect('config-reloaded', () => {
-        hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^(hyprpanel-settings)$"`);
+        hyprlandService.message(`keyword windowrulev2 float, title:^(hyprpanel-settings)$`);
     });
 };
 
 const floatFilePicker = (): void => {
-    hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^((Save|Import) Hyprpanel.*)$"`);
+    hyprlandService.message(`keyword windowrulev2 float, title:^((Save|Import) Hyprpanel.*)$`);
 
     hyprlandService.connect('config-reloaded', () => {
-        hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^((Save|Import) Hyprpanel.*)$"`);
+        hyprlandService.message(`keyword windowrulev2 float, title:^((Save|Import) Hyprpanel.*)$`);
     });
 };
 
