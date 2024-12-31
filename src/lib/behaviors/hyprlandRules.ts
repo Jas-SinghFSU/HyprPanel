5f72b4f5e10ba8b6bb5ccc6a5b6a0136b2223d73
Jas Singh <jaskiratpal.singh@outlook.com>
2024-12-31 01:33:40 -0800

    Workspaces now show up on their appropriate monitors. (#681)
    * Workspaces now show up on their appropriate monitors.
    
    * Fixed undefined rules showing up.

--- src/lib/behaviors/hyprlandRules.ts
+++ src/lib/behaviors/hyprlandRules.ts
@@ -1,19 +1,18 @@
-import { execAsync } from 'astal';
 import { hyprlandService } from '../constants/services';
 
 const floatSettingsDialog = (): void => {
-    execAsync(['bash', '-c', 'hyprctl keyword windowrulev2 "float, title:^(hyprpanel-settings)$"']);
+    hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^(hyprpanel-settings)$"`);
 
     hyprlandService.connect('config-reloaded', () => {
-        execAsync(['bash', '-c', 'hyprctl keyword windowrulev2 "float, title:^(hyprpanel-settings)$"']);
+        hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^(hyprpanel-settings)$"`);
     });
 };
 
 const floatFilePicker = (): void => {
-    execAsync(['bash', '-c', 'hyprctl keyword windowrulev2 "float, title:^((Save|Import) Hyprpanel.*)$"']);
+    hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^((Save|Import) Hyprpanel.*)$"`);
 
     hyprlandService.connect('config-reloaded', () => {
-        execAsync(['bash', '-c', 'hyprctl keyword windowrulev2 "float, title:^((Save|Import) Hyprpanel.*)$"']);
+        hyprlandService.message(`hyprctl keyword windowrulev2 "float, title:^((Save|Import) Hyprpanel.*)$"`);
     });
 };
 
