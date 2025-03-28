303aabb62c485b038d9d3821cd473fb8fe07ce8d
Jas Singh <jaskiratpal.singh@outlook.com>
2025-03-28 01:41:03 -0700

    Feat: Improved GDK<->Hyprland monitor mapping logic.

--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -34,7 +34,8 @@ import { App, Gtk } from 'astal/gtk3';
 
 import Astal from 'gi://Astal?version=3.0';
 import { bind, Variable } from 'astal';
-import { gdkMonitorIdToHyprlandId, getLayoutForMonitor, isLayoutEmpty } from './utils/monitors';
+import { getLayoutForMonitor, isLayoutEmpty } from './utils/monitors';
+import { GdkMonitorMapper } from './utils/GdkMonitorMapper';
 
 const { layouts } = options.bar;
 const { location } = options.theme.bar;
@@ -68,112 +69,110 @@ const widget = {
     cava: (): JSX.Element => WidgetContainer(Cava()),
 };
 
-export const Bar = (() => {
-    const usedHyprlandMonitors = new Set<number>();
+const gdkMonitorMapper = new GdkMonitorMapper();
 
-    return (monitor: number): JSX.Element => {
-        const hyprlandMonitor = gdkMonitorIdToHyprlandId(monitor, usedHyprlandMonitors);
+export const Bar = (monitor: number): JSX.Element => {
+    const hyprlandMonitor = gdkMonitorMapper.mapGdkToHyprland(monitor);
 
-        const computeVisibility = bind(layouts).as(() => {
-            const foundLayout = getLayoutForMonitor(hyprlandMonitor, layouts.get());
-            return !isLayoutEmpty(foundLayout);
-        });
+    const computeVisibility = bind(layouts).as(() => {
+        const foundLayout = getLayoutForMonitor(hyprlandMonitor, layouts.get());
+        return !isLayoutEmpty(foundLayout);
+    });
 
-        const computeClassName = bind(layouts).as(() => {
-            const foundLayout = getLayoutForMonitor(hyprlandMonitor, layouts.get());
-            return !isLayoutEmpty(foundLayout) ? `bar` : '';
-        });
+    const computeClassName = bind(layouts).as(() => {
+        const foundLayout = getLayoutForMonitor(hyprlandMonitor, layouts.get());
+        return !isLayoutEmpty(foundLayout) ? `bar` : '';
+    });
 
-        const computeAnchor = bind(location).as((loc) => {
-            if (loc === 'bottom') {
-                return Astal.WindowAnchor.BOTTOM | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT;
-            }
+    const computeAnchor = bind(location).as((loc) => {
+        if (loc === 'bottom') {
+            return Astal.WindowAnchor.BOTTOM | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT;
+        }
 
-            return Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT;
-        });
+        return Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT;
+    });
 
-        const computeLayer = Variable.derive([bind(options.theme.bar.layer), bind(options.tear)], (barLayer, tear) => {
-            if (tear && barLayer === 'overlay') {
-                return Astal.Layer.TOP;
-            }
-            const layerMap = {
-                overlay: Astal.Layer.OVERLAY,
-                top: Astal.Layer.TOP,
-                bottom: Astal.Layer.BOTTOM,
-                background: Astal.Layer.BACKGROUND,
-            };
+    const computeLayer = Variable.derive([bind(options.theme.bar.layer), bind(options.tear)], (barLayer, tear) => {
+        if (tear && barLayer === 'overlay') {
+            return Astal.Layer.TOP;
+        }
+        const layerMap = {
+            overlay: Astal.Layer.OVERLAY,
+            top: Astal.Layer.TOP,
+            bottom: Astal.Layer.BOTTOM,
+            background: Astal.Layer.BACKGROUND,
+        };
 
-            return layerMap[barLayer];
-        });
+        return layerMap[barLayer];
+    });
 
-        const computeBorderLocation = bind(borderLocation).as((brdrLcn) =>
-            brdrLcn !== 'none' ? 'bar-panel withBorder' : 'bar-panel',
-        );
+    const computeBorderLocation = bind(borderLocation).as((brdrLcn) =>
+        brdrLcn !== 'none' ? 'bar-panel withBorder' : 'bar-panel',
+    );
 
-        const leftBinding = Variable.derive([bind(layouts)], (currentLayouts) => {
-            const foundLayout = getLayoutForMonitor(hyprlandMonitor, currentLayouts);
+    const leftBinding = Variable.derive([bind(layouts)], (currentLayouts) => {
+        const foundLayout = getLayoutForMonitor(hyprlandMonitor, currentLayouts);
 
-            return foundLayout.left
-                .filter((mod) => Object.keys(widget).includes(mod))
-                .map((w) => widget[w](hyprlandMonitor));
-        });
-        const middleBinding = Variable.derive([bind(layouts)], (currentLayouts) => {
-            const foundLayout = getLayoutForMonitor(hyprlandMonitor, currentLayouts);
+        return foundLayout.left
+            .filter((mod) => Object.keys(widget).includes(mod))
+            .map((w) => widget[w](hyprlandMonitor));
+    });
+    const middleBinding = Variable.derive([bind(layouts)], (currentLayouts) => {
+        const foundLayout = getLayoutForMonitor(hyprlandMonitor, currentLayouts);
 
-            return foundLayout.middle
-                .filter((mod) => Object.keys(widget).includes(mod))
-                .map((w) => widget[w](hyprlandMonitor));
-        });
-        const rightBinding = Variable.derive([bind(layouts)], (currentLayouts) => {
-            const foundLayout = getLayoutForMonitor(hyprlandMonitor, currentLayouts);
+        return foundLayout.middle
+            .filter((mod) => Object.keys(widget).includes(mod))
+            .map((w) => widget[w](hyprlandMonitor));
+    });
+    const rightBinding = Variable.derive([bind(layouts)], (currentLayouts) => {
+        const foundLayout = getLayoutForMonitor(hyprlandMonitor, currentLayouts);
 
-            return foundLayout.right
-                .filter((mod) => Object.keys(widget).includes(mod))
-                .map((w) => widget[w](hyprlandMonitor));
-        });
+        return foundLayout.right
+            .filter((mod) => Object.keys(widget).includes(mod))
+            .map((w) => widget[w](hyprlandMonitor));
+    });
 
-        return (
-            <window
-                inhibit={bind(idleInhibit)}
-                name={`bar-${hyprlandMonitor}`}
-                namespace={`bar-${hyprlandMonitor}`}
-                className={computeClassName}
-                application={App}
-                monitor={monitor}
-                visible={computeVisibility}
-                anchor={computeAnchor}
-                layer={computeLayer()}
-                exclusivity={Astal.Exclusivity.EXCLUSIVE}
-                onDestroy={() => {
-                    computeLayer.drop();
-                    leftBinding.drop();
-                    middleBinding.drop();
-                    rightBinding.drop();
-                }}
-            >
-                <box className={'bar-panel-container'}>
-                    <centerbox
-                        css={'padding: 1px;'}
-                        hexpand
-                        className={computeBorderLocation}
-                        startWidget={
-                            <box className={'box-left'} hexpand>
-                                {leftBinding()}
-                            </box>
-                        }
-                        centerWidget={
-                            <box className={'box-center'} halign={Gtk.Align.CENTER}>
-                                {middleBinding()}
-                            </box>
-                        }
-                        endWidget={
-                            <box className={'box-right'} halign={Gtk.Align.END}>
-                                {rightBinding()}
-                            </box>
-                        }
-                    />
-                </box>
-            </window>
-        );
-    };
-})();
+    return (
+        <window
+            inhibit={bind(idleInhibit)}
+            name={`bar-${hyprlandMonitor}`}
+            namespace={`bar-${hyprlandMonitor}`}
+            className={computeClassName}
+            application={App}
+            monitor={monitor}
+            visible={computeVisibility}
+            anchor={computeAnchor}
+            layer={computeLayer()}
+            exclusivity={Astal.Exclusivity.EXCLUSIVE}
+            onDestroy={() => {
+                computeLayer.drop();
+                leftBinding.drop();
+                middleBinding.drop();
+                rightBinding.drop();
+            }}
+        >
+            <box className={'bar-panel-container'}>
+                <centerbox
+                    css={'padding: 1px;'}
+                    hexpand
+                    className={computeBorderLocation}
+                    startWidget={
+                        <box className={'box-left'} hexpand>
+                            {leftBinding()}
+                        </box>
+                    }
+                    centerWidget={
+                        <box className={'box-center'} halign={Gtk.Align.CENTER}>
+                            {middleBinding()}
+                        </box>
+                    }
+                    endWidget={
+                        <box className={'box-right'} halign={Gtk.Align.END}>
+                            {rightBinding()}
+                        </box>
+                    }
+                />
+            </box>
+        </window>
+    );
+};
