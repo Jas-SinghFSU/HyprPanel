2a518ef5f7a0b02dc6be67eab2fe136abe1bc09c
Pabcihba <pabcihba@gmail.com>
2025-04-27 09:10:37 +0300

    Better update script

--- scripts/runUpdates.sh
+++ scripts/runUpdates.sh
@@ -6,10 +6,19 @@ install_arch_updates() {
     echo "Updating Arch Linux system..."
     sudo pacman -Syu
     echo "Updating AUR packages..."
+    if command -v paru &> /dev/null; then
     paru -Syu
+    elif command -v yay &> /dev/null; then
+    yay -Syu
+    # Если ни один хелпер не найден
+    else
+    echo "Missing AUR Helper. Try installing yay or paru"
+    fi
     echo "Updating Flatpak packages..."
     flatpak update -y
-    echo "Done with Arch & AUR updates."
+    echo "Done with Arch & AUR updates."   
+    echo "Press any key to exit..."
+    read -n 1
 }
 
 install_ubuntu_updates() {
@@ -18,6 +27,8 @@ install_ubuntu_updates() {
     echo "Updating Flatpak packages..."
     flatpak update -y
     echo "Done with Ubuntu updates."
+    echo "Press any key to exit..."
+    read -n 1
 }
 
 install_fedora_updates() {
@@ -26,12 +37,16 @@ install_fedora_updates() {
     echo "Updating Flatpak packages..."
     flatpak update -y
     echo "Done with Fedora updates."
+    echo "Press any key to exit..."
+    read -n 1
 }
 
 install_flatpak_updates() {
     echo "Updating Flatpak packages..."
     flatpak update -y
     echo "Done with FlatPak updates."
+    echo "Press any key to exit..."
+    read -n 1
 }
 
 case "$1" in
@@ -52,5 +67,4 @@ case "$1" in
     ;;
 esac
 
-echo "Press any key to exit..."
-read -n 1
+exit 1
