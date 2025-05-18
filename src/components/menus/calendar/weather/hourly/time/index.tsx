6273ee9be7fd36aedfc5263ecc4a58f3d8f34ba3
Jas Singh <jaskiratpal.singh@outlook.com>
2025-05-17 18:40:07 -0700

    Rebase master merge

--- src/components/menus/calendar/weather/hourly/time/index.tsx
+++ src/components/menus/calendar/weather/hourly/time/index.tsx
@@ -1,29 +1,33 @@
 import options from 'src/options';
-import { globalWeatherVar } from 'src/shared/weather';
 import { getNextEpoch } from '../helpers';
 import { bind, Variable } from 'astal';
+import WeatherManager from 'src/shared/weather';
 
+const weatherManager = WeatherManager.get_default();
 const { military } = options.menus.clock.time;
 
 export const HourlyTime = ({ hoursFromNow }: HourlyTimeProps): JSX.Element => {
-    const weatherBinding = Variable.derive([bind(globalWeatherVar), bind(military)], (weather, military) => {
-        if (!Object.keys(weather).length) {
-            return '-';
-        }
+    const weatherBinding = Variable.derive(
+        [bind(weatherManager.weatherData), bind(military)],
+        (weather, military) => {
+            if (!Object.keys(weather).length) {
+                return '-';
+            }
 
-        const nextEpoch = getNextEpoch(weather, hoursFromNow);
-        const dateAtEpoch = new Date(nextEpoch * 1000);
+            const nextEpoch = getNextEpoch(weather, hoursFromNow);
+            const dateAtEpoch = new Date(nextEpoch * 1000);
 
-        let hours = dateAtEpoch.getHours();
+            let hours = dateAtEpoch.getHours();
 
-        if (military) {
-            return `${hours}:00`;
-        }
+            if (military) {
+                return `${hours}:00`;
+            }
 
-        const ampm = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12 || 12;
-        return `${hours}${ampm}`;
-    });
+            const ampm = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12 || 12;
+            return `${hours}${ampm}`;
+        },
+    );
 
     return (
         <label
