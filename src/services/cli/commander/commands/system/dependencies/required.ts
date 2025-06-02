f6399348ab46fe449b21ffc7244588045ad96f10
Jas Singh <jaskiratpal.singh@outlook.com>
2025-06-01 23:01:41 -0700

    Chore: Add missing dependencies to the check dependencies command.

---
+++ src/services/cli/commander/commands/system/dependencies/required.ts
@@ -0,0 +1,76 @@
+import { Dependency } from './types';
+
+export const requiredDependencies: Dependency[] = [
+    {
+        package: 'wireplumber',
+        required: true,
+        type: 'executable',
+        check: ['wireplumber'],
+    },
+    {
+        package: 'libgtop',
+        required: true,
+        type: 'library',
+        check: ['gtop-2.0'],
+    },
+    {
+        package: 'bluez',
+        required: true,
+        type: 'service',
+        check: ['bluetooth.service'],
+    },
+    {
+        package: 'bluez-utils',
+        required: true,
+        type: 'executable',
+        check: ['bluetoothctl'],
+    },
+    {
+        package: 'networkmanager',
+        required: true,
+        type: 'service',
+        check: ['NetworkManager.service'],
+    },
+    {
+        package: 'dart-sass',
+        required: true,
+        type: 'executable',
+        check: ['sass'],
+    },
+    {
+        package: 'wl-clipboard',
+        required: true,
+        type: 'executable',
+        check: ['wl-copy', 'wl-paste'],
+    },
+    {
+        package: 'upower',
+        required: true,
+        type: 'service',
+        check: ['upower.service'],
+    },
+    {
+        package: 'gvfs',
+        required: true,
+        type: 'executable',
+        check: ['/usr/lib/gvfsd', '/usr/libexec/gvfsd', '/usr/lib/gvfs/gvfsd'],
+    },
+    {
+        package: 'gtksourceview3',
+        required: true,
+        type: 'library',
+        check: ['gtksourceview-3.0', 'libgtksourceview-3.0'],
+    },
+    {
+        package: 'libsoup3',
+        required: true,
+        type: 'library',
+        check: ['libsoup-3.0', 'libsoup3'],
+    },
+    {
+        package: 'aylurs-gtk-shell',
+        required: true,
+        type: 'executable',
+        check: ['ags'],
+    },
+];
