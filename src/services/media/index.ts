5edd9734d3a95edd589363aebbf991147508a9f1
davfsa <davfsa@gmail.com>
2025-06-02 11:00:13 +0200

    fix: initialize media players on startup
    Signed-off-by: davfsa <davfsa@gmail.com>

--- src/services/media/index.ts
+++ src/services/media/index.ts
@@ -57,6 +57,10 @@ export class MediaPlayerService {
 
         this.mediaTitle.set(noMediaText.get());
 
+        for (const player of this._mprisService.players) {
+            this._handlePlayerAdded(player);
+        }
+
         this._mprisService.connect('player-closed', (_, closedPlayer) =>
             this._handlePlayerClosed(closedPlayer),
         );
